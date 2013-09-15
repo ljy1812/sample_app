86de1c136ddffe1c9a1413aef9dc88021f15195e
vagrant <vagrant@precise64.(none)>
2013-09-15 13:43:31 +0000

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,16 @@ describe User do
 
   it {should be_valid }
 
+  describe "before save to database checkout the email is downcase" do
+    let(:mixed_case_email){"Foo@ExAMple.CoM"}
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user){User.find_by(email:@user.email)}
@@ -65,7 +75,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[foo@bar..com user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
